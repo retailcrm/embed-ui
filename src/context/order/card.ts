10b2598dc7c585f0b3762100676e9d2af289818e
perminov_dd <danil-perminov7@mail.ru>
2024-11-19 17:39:59 +0300

    feat: ref #98287 Completed order/card with data

--- src/context/order/card.ts
+++ src/context/order/card.ts
@@ -4,17 +4,118 @@ import { defineContext } from '@/context/store'
 
 import {
   isNull,
+  isNumber,
   isString,
   oneOf,
 } from '@/predicates'
 
 export const schema: Schema = {
-  'customer.email': {
+  'id': {
+    accepts: oneOf(isNumber, isNull),
+    defaults: () => null,
+    readonly: true,
+  },
+  'externalId': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: true,
+  },
+  'number': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: true,
+  },
+  'customer.type': {
+    accepts: oneOf(isNumber, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'customer.lastName': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'customer.firstName': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'customer.patronymic': {
     accepts: oneOf(isString, isNull),
     defaults: () => null,
     readonly: false,
   },
   'customer.phone': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'customer.email': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'country': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: true,
+  },
+  'currency': {
+    accepts: isString,
+    defaults: () => '',
+    readonly: true,
+  },
+  'status': {
+    accepts: isString,
+    defaults: () => '',
+    readonly: true,
+  },
+  'company.name': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.legalName': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.legalAddress': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.INN': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.OKPO': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.BIK': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.bank': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.bankAddress': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.corrAccount': {
+    accepts: oneOf(isString, isNull),
+    defaults: () => null,
+    readonly: false,
+  },
+  'company.bankAccount': {
     accepts: oneOf(isString, isNull),
     defaults: () => null,
     readonly: false,
