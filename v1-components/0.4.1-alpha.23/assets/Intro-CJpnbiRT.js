a4ba3c4c286e67116e7b90b95002450ecc904a18
cmath10 <cmath10@users.noreply.github.com>
2024-11-28 11:01:16 +0000

    deploy: 8e6af39160d1d35ca0dbeed6ff4b6fce9d12e297

---
+++ v1-components/0.4.1-alpha.23/assets/Intro-CJpnbiRT.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-DQFc-wkB.js";import{useMDXComponents as i}from"./index-BjypYOp6.js";import"./_commonjsHelpers-Cpj98o6Y.js";function r(l){const n={a:"a",h1:"h1",li:"li",p:"p",ul:"ul",...i(),...l.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{id:"галерея-vue-компонентов-для-js-модулей",children:"Галерея Vue-компонентов для JS-модулей"}),`
+`,e.jsx(n.p,{children:"В данной галерее представлены Vue-компоненты из интерфейса RetailCRM / Simla.com, которые доступны для использования в JS-модулях."}),`
+`,e.jsx(n.p,{children:"Другие полезные материалы:"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://github.com/retailcrm/embed-ui",rel:"nofollow",children:"@retailcrm/embed-ui"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://docs.retailcrm.ru/Developers/modules/PublishingModuleMarketplace/JsModules",rel:"nofollow",children:"Документация по разработке JS-модулей"})}),`
+`,e.jsxs(n.li,{children:["Примеры JS-модулей ",e.jsx(n.a,{href:"https://github.com/retailcrm/core-ui-extensions-examples",rel:"nofollow",children:"@retailcrm/core-ui-extensions-examples"})]}),`
+`]})]})}function c(l={}){const{wrapper:n}={...i(),...l.components};return n?e.jsx(n,{...l,children:e.jsx(r,{...l})}):r(l)}export{c as default};
