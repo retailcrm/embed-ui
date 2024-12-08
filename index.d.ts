55cfad86aaef4c1a478a88ca1582a4dcf157a676
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2024-12-08 14:34:58 +0400

    refactor: index.d.ts declaration now re-exports types ContextStore & ContextStoreDefinition from v1-contexts

--- index.d.ts
+++ index.d.ts
@@ -10,13 +10,17 @@ import type {
 } from '@remote-ui/rpc'
 
 import type {
-  Context,
   ContextAccessor,
   ContextSchema,
   IsReadonly,
   RejectionHandler,
   TypeOf,
 } from '@retailcrm/embed-ui-v1-types/context'
+
+import type {
+  ContextStore,
+  ContextStoreDefinition,
+} from '@retailcrm/embed-ui-v1-contexts/remote'
 
 import type { Schema as CustomerCardSchema } from '@retailcrm/embed-ui-v1-contexts/types/customer/card'
 import type { Schema as CustomerCardPhoneSchema } from '@retailcrm/embed-ui-v1-contexts/types/customer/card-phone'
@@ -26,9 +30,6 @@ import type { Schema as SettingsSchema } from '@retailcrm/embed-ui-v1-contexts/t
 
 import type { SchemaList } from '@retailcrm/embed-ui-v1-contexts/types'
 
-import type { Store } from 'pinia'
-import type { StoreDefinition } from 'pinia'
-
 import type { Callable } from './types/host/callable'
 import type { WidgetRunner } from './types/widget'
 
@@ -37,22 +38,8 @@ export declare const createWidgetEndpoint: (
   messenger: MessageEndpoint
 ) => Endpoint<ContextAccessor<SchemaList> & Callable>
 
-export type ContextStore<S extends ContextSchema> = Store<string, Context<S>, {
-  schema(): S;
-}, {
-  initialize(): Promise<void>;
-  set<F extends keyof S>(field: F, value: TypeOf<S[F]>): void;
-}>
-
-export type ContextStoreDefinition<
-  Id extends string,
-  S extends ContextSchema
-> = StoreDefinition<Id, Context<S>, {
-  schema(): S;
-}, {
-  initialize(): Promise<void>;
-  set<F extends keyof S>(field: F, value: TypeOf<S[F]>): void;
-}>
+export type { ContextStore }
+export type { ContextStoreDefinition }
 
 export declare const useField: <S extends ContextSchema, F extends keyof S>(
   store: ContextStore<S>,
