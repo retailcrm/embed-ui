066b9c81482d6b0f9c9f946c7a16877d52d60495
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2025-01-20 15:35:22 +0400

    feat(v1-contexts): Added field 'image.workers' to settings context

--- packages/v1-contexts/types/settings.d.ts
+++ packages/v1-contexts/types/settings.d.ts
@@ -2,5 +2,6 @@ import type { ReadonlyField } from '@retailcrm/embed-ui-v1-types/context'
 
 export type Locale = 'en-GB' | 'es-ES' | 'ru-RU'
 export type Schema = {
+  'image.workers': ReadonlyField<string[]>;
   'system.locale': ReadonlyField<Locale>;
 }
