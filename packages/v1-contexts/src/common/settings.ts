c76c20e65446bf3d1df0b7d4a9f19abc17d7cc76
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2025-01-23 13:55:26 +0400

    feat(v1-contexts): Added field for system routing data

--- packages/v1-contexts/src/common/settings.ts
+++ packages/v1-contexts/src/common/settings.ts
@@ -9,15 +9,75 @@ import type {
 } from '~types/settings'
 
 import {
+  Route,
+  RoutesMap,
+  RoutingData,
+} from '@omnicajs/symfony-router'
+
+import {
   arrayOf,
-  isString,
+  isArray,
+  isBoolean,
   isExactly,
+  isObject,
+  isString,
   oneOf,
+  withMeta,
 } from '@/predicates'
+
+import { keysOf } from '@/utilities'
 
 export const id = 'settings'
 
 export const locales = ['en-GB', 'es-ES', 'ru-RU'] as Locale[]
+
+const routeSchema = {
+  tokens: arrayOf(arrayOf(oneOf(isString, isBoolean))),
+  defaults: oneOf(isArray, isObject),
+  requirements: oneOf(isArray, isObject),
+  hosttokens: arrayOf(arrayOf(isString)),
+  schemes: arrayOf(isString),
+  methods: arrayOf(isString),
+} satisfies {
+  [p in keyof Route]: (value: unknown) => boolean;
+}
+
+const isRoute = (value: unknown): value is Route => {
+  return typeof value === 'object'
+    && value !== null
+    && keysOf(routeSchema).every(key => {
+      return key in value && routeSchema[key](value[key as keyof typeof value])
+    })
+}
+
+const isRoutesMap = (value: unknown): value is RoutesMap => {
+  return isObject(value) && keysOf(value).every(key => isRoute(value[key]))
+}
+
+const routingDataSchema = {
+  base_url: [true, isString],
+  routes: [true, isRoutesMap],
+  prefix: [false, isString],
+  host: [true, isString],
+  port: [false, isString],
+  scheme: [false, isString],
+  locale: [false, isString],
+} satisfies {
+  [p in keyof RoutingData]: [
+    required: boolean,
+    predicate: (value: unknown) => boolean
+  ];
+}
+
+export const routingDataDefaults = {
+  base_url: '',
+  routes: {},
+  prefix: '',
+  host: '',
+  port: '',
+  scheme: 'http',
+  locale: '',
+} satisfies RoutingData
 
 export const schema: Schema = {
   'image.workers': {
@@ -28,6 +88,22 @@ export const schema: Schema = {
   'system.locale': {
     accepts: oneOf(...locales.map(isExactly)),
     defaults: () => 'en-GB',
+    readonly: true,
+  },
+  'system.routing': {
+    accepts: withMeta((value): value is RoutingData => {
+      return typeof value === 'object'
+        && value !== null
+        && keysOf(routingDataSchema).every(key => {
+          const [required, predicate] = routingDataSchema[key]
+          if (!(key in value)) {
+            return !required
+          }
+
+          return predicate(value[key as keyof typeof value])
+        })
+    }, 'RoutingData'),
+    defaults: () => ({ ...routingDataDefaults, routes: {} }),
     readonly: true,
   },
 }
@@ -45,6 +121,13 @@ export const description: ContextSchemaDescription<Schema> = {
       'en-GB': 'Current system\'s locale',
       'es-ES': 'Configuración regional actual del sistema',
       'ru-RU': 'Текущая локаль системы',
+    },
+  },
+  'system.routing': {
+    description: {
+      'en-GB': 'Data for Symfony\'s JS router',
+      'es-ES': 'Datos para el enrutador JS de Symfony',
+      'ru-RU': 'Данные для js-маршрутизатора symfony',
     },
   },
 }
--- packages/v1-contexts/types/settings.d.ts
+++ packages/v1-contexts/types/settings.d.ts
@@ -1,7 +1,9 @@
 import type { ReadonlyField } from '@retailcrm/embed-ui-v1-types/context'
+import type { RoutingData } from '@omnicajs/symfony-router'
 
 export type Locale = 'en-GB' | 'es-ES' | 'ru-RU'
 export type Schema = {
   'image.workers': ReadonlyField<string[]>;
   'system.locale': ReadonlyField<Locale>;
+  'system.routing': ReadonlyField<RoutingData>;
 }
