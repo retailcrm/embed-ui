ca63bdf58221b884293a0a88b0230ed6cf3ec03c
cmath10 <cmath10@users.noreply.github.com>
2025-11-05 14:19:49 +0000

    deploy: e3fe25e90cde30a347d025ad0736da7c131fd3ed

---
+++ v1-components/0.9.1-alpha.1/assets/composables-CTw0CB0X.js
@@ -0,0 +1,1 @@
+import{b as o,r as e,p as r}from"./vue.esm-bundler-DRrU9--T.js";const n=Symbol.for("[@retailcrm/embed-ui-v1-components]:ElementRef"),s=()=>{const t=o(n,e(null));return r(n,e(null)),t};export{n as E,s as u};
