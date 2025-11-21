63aa4f6172469f542cacfc54ddad24848f3bba48
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2025-11-21 13:47:56 +0400

    feat: Added goTo method to host callables for navigation inside CRM

--- types/host/callable.d.ts
+++ types/host/callable.d.ts
@@ -1,6 +1,8 @@
 import type { Pojo } from '@retailcrm/embed-ui-v1-types/scaffolding'
 
 export type Callable = {
+  goTo (route: string, params?: Record<string, unknown>): void;
+
   httpCall (
     action: string,
     payload?: string | Pojo
