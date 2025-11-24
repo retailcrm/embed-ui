c1ac97499a4d97e4e7737bd2a6226f0e85af5804
RetailCRM.CI <integration@retailcrm.ru>
2025-11-24 06:49:42 +0000

    Update latest symlink to 0.9.1

--- v1-components/latest/assets/plugin-bqpY9d2u.js
+++
@@ -1,1 +0,0 @@
-import"./vue.esm-bundler-DRrU9--T.js";const n=Symbol("$embedI18n");export{n as I};
--- v1-components/latest/assets/preview-Dh5OBlxm.js
+++
@@ -1,1 +0,0 @@
-import"./vue.esm-bundler-DRrU9--T.js";const o=Symbol("$image.workers");export{o as I};
