5054dd700948770b1eecabd37f9ec67a32ab4e55
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2026-02-12 10:49:30 +0400

    fix(v1-components): apply vue plugins to storybook worker build

--- packages/v1-components/storybook/vite.config.ts
+++ packages/v1-components/storybook/vite.config.ts
@@ -2,7 +2,16 @@ import {
   defineConfig,
   mergeConfig,
 } from 'vite'
+import svg from 'vite-svg-loader'
+import vue from '@vitejs/plugin-vue'
 
 import common from '../vite.config.basic'
 
-export default mergeConfig(common, defineConfig({}))
+export default mergeConfig(common, defineConfig({
+  worker: {
+    plugins: () => [
+      svg(),
+      vue(),
+    ],
+  },
+}))
