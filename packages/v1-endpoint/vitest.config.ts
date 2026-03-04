2266325e386725fb84dc6f4591fd2003af053297
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2026-03-04 17:08:53 +0400

    fix(v1-endpoint): Build and test workspace configuration were corrected

--- packages/v1-endpoint/vitest.config.ts
+++ packages/v1-endpoint/vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    exclude: ['packages/v1-endpoint/tests/**/*.e2e.ts'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      'packages/v1-endpoint/tests/**/*.e2e.ts',
+    ],
   },
 })
