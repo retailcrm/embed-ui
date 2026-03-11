75dd5fbca900d6f2c4c3b63e0bdc182243677eb1
RetailCRM.CI <integration@retailcrm.ru>
2026-03-11 08:10:08 +0000

    Update latest symlink to 0.9.11

--- v1-components/latest/assets/formatter-QJ4M4OGQ-ClukM3jo.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as e}from"./blocks-CkPvKPWb.js";import{e as o}from"./iframe-BHh6c9lZ.js";import"./preload-helper-PPVm8Dsz.js";import"./index-LVSkv26z.js";import"./jsx-runtime-BGuG-3rP.js";var t=o(e(),1),_=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
--- v1-components/latest/assets/plugin-CqJxYN_5.js
+++
@@ -1,1 +0,0 @@
-import"./iframe-BHh6c9lZ.js";const n=Symbol("$embedI18n");export{n as I};
