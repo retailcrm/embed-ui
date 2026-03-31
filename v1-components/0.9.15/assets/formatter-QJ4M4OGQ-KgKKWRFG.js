f101c507e8c18bac6a708f3353338b27c090b5cb
cmath10 <cmath10@users.noreply.github.com>
2026-03-31 11:42:57 +0000

    deploy: 57a5d8cefe064a82152410cc68cdb271d7e7f7c9

---
+++ v1-components/0.9.15/assets/formatter-QJ4M4OGQ-KgKKWRFG.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./blocks-DxbhbOcf.js";import{e as o}from"./iframe-7PfFZbMo.js";import"./preload-helper-PPVm8Dsz.js";import"./index-a22oIHiu.js";import"./jsx-runtime-oUFsCJCe.js";var t=o(e(),1),_=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
