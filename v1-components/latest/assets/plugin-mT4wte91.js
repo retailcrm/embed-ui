2800f846f8f4bf5494317f59e2f38a6eade1a5d6
RetailCRM.CI <integration@retailcrm.ru>
2026-03-31 11:43:02 +0000

    Update latest symlink to 0.9.15

---
+++ v1-components/latest/assets/plugin-mT4wte91.js
@@ -0,0 +1,1 @@
+import"./iframe-7PfFZbMo.js";const n=Symbol("$embedI18n");export{n as I};
---
+++ v1-components/latest/assets/tooltip-D7-IeKmg.js
@@ -0,0 +1,1 @@
+import{d as o}from"./createRemoteStoryRender-Bj7plM2v.js";const t="UiTooltip",e=o(t);export{e as U};
